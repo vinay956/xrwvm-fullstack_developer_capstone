a7bbdccd7184db89717b6254e61332b5ce6f047d
vinay956 <vinaykumarbasava@gmail.com>
2024-08-24 07:59:53 -0400

    Modles Added as Per Module 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,17 +1,40 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(null=True, max_length=500)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
+
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, null=True, on_delete = models.CASCADE)
+
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "WAGON"
+    TYPE_CHOICES = (
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+    )
+    model_type  = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    year = models.DateField()
+
+    def __str__(self):
+        return f"{self.make} {self.name} ({self.year})"
+
+        
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
