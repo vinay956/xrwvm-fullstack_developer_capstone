7e4750f66bac96b9dc7939a9e876dc498aeecec6
VinayKumar Basava <44405756+vinay956@users.noreply.github.com>
2024-08-25 12:40:25 +0530

    Fixed lint errors urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,26 +1,37 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
-    path('registration/', views.registration, name='registration'),
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
+urlpatterns = [
+    # path for registration
+    path('registration/', views.registration, name='registration'),
+
+    # path for login
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('get_cars/', views.get_cars, name='getcars'),
 
     # path for dealer reviews view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path(
+        'get_dealers/<str:state>/',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        'dealer/<int:dealer_id>/',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        'reviews/dealer/<int:dealer_id>/',
+        views.get_dealer_reviews,
+        name='dealer_details'
+    ),
 
     # path for add a review view
-    path(route='add_review', view=views.add_review, name='add_review'),
-
+    path('add_review/', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
