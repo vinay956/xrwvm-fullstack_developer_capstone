142c6606a163120bd95fde1fdedaddf3b28e4101
VinayKumar Basava <44405756+vinay956@users.noreply.github.com>
2024-08-26 22:37:30 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,129 +1,193 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+# from .models import related models
+from .models import CarDealer, DealerReview, CarModel
+# from .restapis import related methods
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from datetime import datetime
 import logging
 import json
-from django.http import JsonResponse
-from django.contrib.auth import logout, login, authenticate
-from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
-from .models import CarMake, CarModel
-from .populate import initiate
-from .utils import get_request, analyze_review_sentiments  # Removed unused `post_review`
-
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, get_dealer_by_id, post_request
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-@csrf_exempt
-def registration(request):
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    username_exist = False
 
-    try:
-        # Check if user already exists
-        User.objects.get(username=username)
-        username_exist = True
-    except User.DoesNotExist:
-        # If not, simply log this is a new user
-        logger.debug(f"{username} is a new user")
 
-    # If it is a new user
-    if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-            email=email
-        )
-        # Login the user and redirect to list page
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-        return JsonResponse(data)
+
+
+# Create an `about` view to render a static about page
+# def about(request):
+# ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
+
+# Create a `contact` view to return a static contact page
+#def contact(request):
+
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact_us.html', context)
+# Create a `login_request` view to handle sign in request
+# def login_request(request):
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # If not, return to login page again
+            return render(request, 'djangoapp/user_login.html', context)
     else:
-        data = {"userName": username, "error": "Already Registered"}
-        return JsonResponse(data)
+        return render(request, 'djangoapp/user_login.html', context)
 
-
-# Create a `login_user` view to handle sign-in request
-@csrf_exempt
-def login_user(request):
-    # Get username and password from request.POST dictionary
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    # Try to check if provided credentials can be authenticated
-    user = authenticate(username=username, password=password)
-    data = {"userName": username}
-    if user is not None:
-        # If user is valid, call login method to login current user
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
-
-
-# Create a `logout_request` view to handle sign-out request
+# Create a `logout_request` view to handle sign out request
+# def logout_request(request):
 def logout_request(request):
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
     logout(request)
-    data = {"userName": ""}
-    return JsonResponse(data)
+    # Redirect user back to course list view
+    return redirect('djangoapp:index')
 
-
-# Get Cars by Model
-def get_cars(request):
-    count = CarMake.objects.filter().count()
-    if count == 0:
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = [
-        {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
-        for car_model in car_models
-    ]
-    return JsonResponse({"CarModels": cars})
-
+# Create a `registration_request` view to handle sign up request
+# def registration_request(request):
+def registration_request(request):
+    context = {}
+    # If it is a GET request, just render the registration page
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request, state="All"):
-    endpoint = "/fetchDealers" if state == "All" else f"/fetchDealers/{state}"
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        # Get dealers from the Cloudant DB
+        context["dealerships"] = get_dealers_from_cf()
+
+        # dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
+        # return HttpResponse(dealer_names)
+
+        return render(request, 'djangoapp/index.html', context)
 
 
+
+
+
+# Create a `get_dealer_details` view to render the reviews of a dealer
+# def get_dealer_details(request, dealer_id):
+# ...
+# View to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
-    if dealer_id:
-        endpoint = f"/fetchDealer/{dealer_id}"
-        dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+    context = {}
+    if request.method == "GET":
+        reviews = get_dealer_reviews_from_cf(dealer_id=dealer_id)
+        context = {
+            "reviews":  reviews, 
+            "dealer_id": dealer_id,
+            "dealer": get_dealer_by_id(dealer_id),
+            "cars": CarModel.objects.all()
+        }
+
+        return render(request, 'djangoapp/dealer_details.html', context)
+# Create a `add_review` view to submit a review
+# def add_review(request, dealer_id):
+# ...
+
+# View to submit a new review
+def add_review(request, dealer_id):
+    # User must be logged in before posting a review
+    if request.user.is_authenticated:
+        # GET request renders the page with the form for filling out a review
+        if request.method == "GET":
+            # Get dealer details from the API
+            context = {
+                "cars": CarModel.objects.all(),
+                "dealer": get_dealer_by_id(dealer_id),
+            }
+            return render(request, 'djangoapp/add_review.html', context)
+
+        # POST request posts the content in the review submission form to the Cloudant DB using the post_review Cloud Function
+        if request.method == "POST":
+            form = request.POST
+            review = dict()
+            review["name"] = f"{request.user.first_name} {request.user.last_name}"
+            review["dealership"] = dealer_id
+            review["review"] = form["content"]
+            review['id'] = dealer_id
+            review["purchase"] = form.get("purchasecheck")
+            if review["purchase"]:
+                review["purchase"] = 'yes'
+            else:
+                review["purchase"] = 'No'
+
+            car = CarModel.objects.get(pk=form["car"])
+            review["car_make"] = car.car_make.name
+            review["car_model"] = car.name
+            review["car_year"] = car.year
+             # If the user bought the car, get the purchase date
+            if form.get("purchasecheck"):
+                review["purchase_date"] = form.get("purchasedate")
+                
+            else: 
+                review["purchase_date"] = None
+
+            
+            
+
+            if(review):
+                result = post_request(review) # Create a JSON payload that contains the review data
+           
+            # Performing a POST request with the review
+            
+            if int(result.status_code) == 200:
+                print("Review posted successfully.")
+
+            # After posting the review the user is redirected back to the dealer details page
+            return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
-
-def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
-    if dealer_id:
-        endpoint = f"/fetchReviews/dealer/{dealer_id}"
-        reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
-
-def add_review(request):
-    if not request.user.is_anonymous:
-        data = json.loads(request.body)
-        try:
-            # Assuming post_review logic is implemented elsewhere in the code
-            response = post_review(data)
-            return JsonResponse({"status": 200})
-        except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
-    else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        # If user isn't logged in, redirect to login page
+        print("User must be authenticated before posting a review. Please log in.")
+        return redirect("/djangoapp/login")
